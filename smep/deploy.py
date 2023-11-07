740af9aafa088b510a41282540ef76f50e89cbfa
Ashish Kumar <ashish@ashishkumar.org>
2023-11-07 11:19:23 -0800

    Update deploy.py
    For Neuron SDK 2.14.1

--- smep/deploy.py
+++ smep/deploy.py
@@ -1,56 +1,131 @@
+import io
+import json
 import boto3
-import sagemaker
-from sagemaker import Model, image_uris, serializers, deserializers
 import os
 from datetime import datetime
-MODEL_NAME="smep-inf2-llama2-7b-chat"
+import time 
+
 boto3_session=boto3.session.Session(
     aws_access_key_id=os.environ['AWS_ACCESS_KEY_ID'], 
     aws_secret_access_key=os.environ['AWS_SECRET_ACCESS_KEY'], 
     aws_session_token=os.environ['AWS_SESSION_TOKEN'],
     region_name="us-east-1")
 smr = boto3.client('sagemaker-runtime')
-sm = boto3.client('sagemaker')
-role = 'arn:aws:iam::102048127330:role/service-role/SageMaker-ak-datascientist'  # execution role for the endpoint
-instance_type = "ml.inf2.xlarge"
-endpoint_name = sagemaker.utils.name_from_base(MODEL_NAME)
+endpoint_name = 'smep-inf2-llama2-13b-chat-2023-10-25-19-49-33-570' #"smep-inf2-llama2-13b-b1-2023-10-20-04-53-19-441"
 
-sess = sagemaker.session.Session(boto3_session, sagemaker_client=sm, sagemaker_runtime_client=smr)  # sagemaker session for interacting with different AWS APIs
-region = sess._region_name  # region name of the current SageMaker Studio environment
-account = sess.account_id()  # account_id of the current SageMaker Studio environment
-bucket_name = sess.default_bucket()
-prefix='torchserve'
-output_path = f"s3://{bucket_name}/{prefix}"
-print(f'account={account}, region={region}, role={role}, output_path={output_path}')
-s3_uri = f'{output_path}/model_store/llama-2-7b-chat/' #  "s3://sagemaker-us-east-1-102048127330/torchserve/model_store/llama-2-13b-chat/"
-print("======================================")
-print(f'Will load artifacts from {s3_uri}')
-print("======================================")
-image_uri = '102048127330.dkr.ecr.us-east-1.amazonaws.com/neuronx:2-14-1'
+class Parser:
+    """
+    A helper class for parsing the byte stream input. 
+    
+    The output of the model will be in the following format:
+    ```
+    b'{"outputs": [" a"]}\n'
+    b'{"outputs": [" challenging"]}\n'
+    b'{"outputs": [" problem"]}\n'
+    ...
+    ```
+    
+    While usually each PayloadPart event from the event stream will contain a byte array 
+    with a full json, this is not guaranteed and some of the json objects may be split across
+    PayloadPart events. For example:
+    ```
+    {'PayloadPart': {'Bytes': b'{"outputs": '}}
+    {'PayloadPart': {'Bytes': b'[" problem"]}\n'}}
+    ```
+    
+    This class accounts for this by concatenating bytes written via the 'write' function
+    and then exposing a method which will return lines (ending with a '\n' character) within
+    the buffer via the 'scan_lines' function. It maintains the position of the last read 
+    position to ensure that previous bytes are not exposed again. 
+    """
+    
+    def __init__(self):
+        self.buff = io.BytesIO()
+        self.read_pos = 0
+        
+    def write(self, content):
+        self.buff.seek(0, io.SEEK_END)
+        self.buff.write(content)
+        data = self.buff.getvalue()
+        
+    def scan_lines(self):
+        self.buff.seek(self.read_pos)
+        for line in self.buff.readlines():
+            if line[-1] != b'\n':
+                self.read_pos += len(line)
+                yield line[:-1]
+                
+    def reset(self):
+        self.read_pos = 0
+        
 
+payload = {
+    "inputs": [
+        [
+            {"role": "user", "content": "what is the recipe of mayonnaise?"},
+        ]
+    ],
+    "parameters": {"max_new_tokens": 1024, "top_p": 0.9, "temperature": 0.6},
+}
+start = time.time()
+resp = smr.invoke_endpoint_with_response_stream(EndpointName=endpoint_name, 
+                                                Body=json.dumps(payload), 
+                                                ContentType="application/json")
+event_stream = resp['Body']
+parser = Parser()
+for event in event_stream:
+    parser.write(event['PayloadPart']['Bytes'])
+    for line in parser.scan_lines():
+        print(line.decode("utf-8"), end=' ')
+print('\n')
+end = time.time()
+print(f'Prediction took {end-start} seconds\n')
 
-model = Model(
-    name=MODEL_NAME + datetime.now().strftime("%Y-%m-%d-%H-%M-%S"),
-    # Enable SageMaker uncompressed model artifacts
-    model_data={
-        "S3DataSource": {
-                "S3Uri": s3_uri,
-                "S3DataType": "S3Prefix",
-                "CompressionType": "None",
-        }
-    },
-    image_uri=image_uri,
-    role=role,
-    sagemaker_session=sess,
-    env={"TS_INSTALL_PY_DEP_PER_MODEL": "true"},
-)
-print(model)
+payload2 = {
+    "inputs": [
+        [
+            {"role": "system", "content": "Always answer with Haiku"},
+            {"role": "user", "content": "I am going to Paris, what should I see?"},
+        ]
+    ],
+    "parameters": {"max_new_tokens": 384, "top_p": 0.9, "temperature": 0.6},
+}
+start = time.time()
+resp = smr.invoke_endpoint_with_response_stream(EndpointName=endpoint_name, 
+                                                Body=json.dumps(payload2), 
+                                                ContentType="application/json")
+event_stream = resp['Body']
+parser = Parser()
+for event in event_stream:
+    parser.write(event['PayloadPart']['Bytes'])
+    for line in parser.scan_lines():
+        print(line.decode("utf-8"), end=' ')
+print('\n')
+end = time.time()
+print(f'Prediction took {end-start} seconds\n')
 
-model.deploy(
-    initial_instance_count=1,
-    instance_type=instance_type,
-    endpoint_name=endpoint_name,
-    volume_size=512, # increase the size to store large model
-    model_data_download_timeout=3600, # increase the timeout to download large model
-    container_startup_health_check_timeout=600, # increase the timeout to load large model
-)
+payload3 = payload = {
+    "inputs": [
+        [
+            {
+                "role": "system",
+                "content": "Always answer with emojis",
+            },
+            {"role": "user", "content": "How to go from Beijing to NY?"},
+        ]
+    ],
+    "parameters": {"max_new_tokens": 512, "top_p": 0.9, "temperature": 0.6},
+}
+start = time.time()
+resp = smr.invoke_endpoint_with_response_stream(EndpointName=endpoint_name, 
+                                                Body=json.dumps(payload3), 
+                                                ContentType="application/json")
+event_stream = resp['Body']
+parser = Parser()
+for event in event_stream:
+    parser.write(event['PayloadPart']['Bytes'])
+    for line in parser.scan_lines():
+        print(line.decode("utf-8"), end=' ')
+print('\n')
+end = time.time()
+print(f'Prediction took {end-start} seconds\n')
