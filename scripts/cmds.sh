6856c0438fa660856f75c5f171af60ad479524d7
Ashish Kumar <ashish@ashishkumar.org>
2024-04-23 21:42:53 +0000

    Added instructions for token

--- scripts/cmds.sh
+++ scripts/cmds.sh
@@ -1,6 +1,7 @@
 #!/bin/sh
 
 ## split and save the model 
-export HF_TOKEN=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
+## Update HF_TOKEN value in line 5
+export HF_TOKEN=hf_OImhboCwtKsmGVLpP
 python split_and_save.py --model_name 'meta-llama/Meta-Llama-3-8B-Instruct' --save_path "../2.18.1/model_store/Meta-Llama-3-8B-Instruct/Meta-Llama-3-8B-Instruct-split/"
 python compile.py compile 
